86c89e50fef3eab54640cc91fdfcc42fbbe95510
Reza Mohseni <49490512+mohsenireza@users.noreply.github.com>
2024-10-31 15:05:02 +0330

    Implement home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+
+import { db } from '../../data/db';
+
 function Home() {
-  return <div>List of rooms</div>;
+  return (
+    <ul>
+      {db.rooms.map((room) => (
+        <li key={room.id}>
+          <Link to={`/room/${room.id}`}>
+            <h3>{room.title}</h3>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default Home;
