16ccbb5950eca535431de722f8d7768bf80413b7
Reza Mohseni <49490512+mohsenireza@users.noreply.github.com>
2024-10-31 13:50:46 +0330

    Initialize a fake db

---
+++ src/data/db.js
@@ -0,0 +1,94 @@
+export const db = {
+  rooms: [
+    {
+      id: '1',
+      title: 'Room A',
+      movies: [
+        {
+          id: '1',
+          title: 'The Shawshank Redemption',
+          image: 'https://m.media-amazon.com/images/M/MV5BMDAyY2FhYjctNDc5OS00MDNlLThiMGUtY2UxYWVkNGY2ZjljXkEyXkFqcGc@._V1_QL75_UX140_CR0,1,140,207_.jpg',
+          startsAt: 'This Monday 12:00 AM'
+        },
+        {
+          id: '2',
+          title: 'The Godfather',
+          image: 'https://m.media-amazon.com/images/M/MV5BYTJkNGQyZDgtZDQ0NC00MDM0LWEzZWQtYzUzZDEwMDljZWNjXkEyXkFqcGc@._V1_QL75_UY207_CR3,0,140,207_.jpg',
+          startsAt: 'This Monday 02:00 PM'
+        },
+        {
+          id: '3',
+          title: 'The Dark Knight',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_QL75_UX140_CR0,0,140,207_.jpg',
+          startsAt: 'This Monday 04:30 PM'
+        },
+        {
+          id: '4',
+          title: 'The Godfather Part II',
+          image: 'https://m.media-amazon.com/images/M/MV5BNzc1OWY5MjktZDllMi00ZDEzLWEwMGItYjk1YmRhYjBjNTVlXkEyXkFqcGc@._V1_QL75_UY207_CR3,0,140,207_.jpg',
+          startsAt: 'This Monday 07:00 PM'
+        }
+      ]
+    },
+    {
+      id: '2',
+      title: 'Room B',
+      movies: [
+        {
+          id: '5',
+          title: 'The Lord of the Rings: The Return of the King',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTZkMjBjNWMtZGI5OC00MGU0LTk4ZTItODg2NWM3NTVmNWQ4XkEyXkFqcGc@._V1_QL75_UX140_CR0,0,140,207_.jpg',
+          startsAt: 'This Tuesday 12:00 AM'
+        },
+        {
+          id: '6',
+          title: 'The Lord of the Rings: The Fellowship of the Ring',
+          image: 'https://m.media-amazon.com/images/M/MV5BNzIxMDQ2YTctNDY4MC00ZTRhLTk4ODQtMTVlOWY4NTdiYmMwXkEyXkFqcGc@._V1_QL75_UX140_CR0,0,140,207_.jpg',
+          startsAt: 'This Tuesday 02:00 PM'
+        },
+        {
+          id: '7',
+          title: 'The Lord of the Rings: The Two Towers',
+          image: 'https://m.media-amazon.com/images/M/MV5BMGQxMDdiOWUtYjc1Ni00YzM1LWE2NjMtZTg3Y2JkMjEzMTJjXkEyXkFqcGc@._V1_QL75_UX140_CR0,5,140,207_.jpg',
+          startsAt: 'This Tuesday 04:30 PM'
+        },
+        {
+          id: '8',
+          title: 'Inception',
+          image: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_QL75_UX140_CR0,0,140,207_.jpg',
+          startsAt: 'This Tuesday 07:00 PM'
+        }
+      ]
+    },
+    {
+      id: '3',
+      title: 'Room C',
+      movies: [
+        {
+          id: '9',
+          title: 'The Green Mile',
+          image: 'https://m.media-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_QL75_UX140_CR0,0,140,207_.jpg',
+          startsAt: 'This Wednesday 12:00 AM'
+        },
+        {
+          id: '10',
+          title: 'The Matrix',
+          image: 'https://m.media-amazon.com/images/M/MV5BN2NmN2VhMTQtMDNiOS00NDlhLTliMjgtODE2ZTY0ODQyNDRhXkEyXkFqcGc@._V1_QL75_UX140_CR0,1,140,207_.jpg',
+          startsAt: 'This Wednesday 02:00 PM'
+        },
+        {
+          id: '11',
+          title: 'Goodfellas',
+          image: 'https://m.media-amazon.com/images/M/MV5BN2E5NzI2ZGMtY2VjNi00YTRjLWI1MDUtZGY5OWU1MWJjZjRjXkEyXkFqcGc@._V1_QL75_UX140_CR0,1,140,207_.jpg',
+          startsAt: 'This Wednesday 04:30 PM'
+        },
+        {
+          id: '12',
+          title: 'Interstellar',
+          image: 'https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_QL75_UX140_CR0,0,140,207_.jpg',
+          startsAt: 'This Wednesday 07:00 PM'
+        }
+      ]
+    }
+  ]
+}
