609da792cf33808f97e8381fcefbd230a2bc6dff
Reza Mohseni <49490512+mohsenireza@users.noreply.github.com>
2024-11-04 19:48:49 +0330

    Implement unit test for RoomItem component

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^.+\\.svg$": "jest-svg-transformer",
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy"
+  },
+  setupFilesAfterEnv: [
+    "<rootDir>/setupTests.js"
+  ]
+}
