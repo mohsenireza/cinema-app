8169b46b91123c2855b2964883d3d59c5393837a
Reza Mohseni <49490512+mohsenireza@users.noreply.github.com>
2024-11-04 20:34:07 +0330

    Implement unit tests for MovieItem component

--- src/components/RoomItem/RoomItem.unit.test.js
+++ src/components/RoomItem/RoomItem.unit.test.js
@@ -3,19 +3,16 @@ import userEvent from '@testing-library/user-event';
 import { render, screen } from '../../test/utils';
 import RoomItem from './RoomItem';
 
-test('should render the room item', async () => {
-  const title = 'title';
-  const id = 'id';
+const title = 'title';
+const id = 'id';
 
+test('should render the room item', () => {
   render(<RoomItem id={id} title={title} />);
 
   expect(screen.getByRole('link', { name: title })).toBeInTheDocument();
 });
 
 test('should navigate to the room page when user clicks on a room item', async () => {
-  const title = 'title';
-  const id = 'id';
-
   render(<RoomItem id={id} title={title} />);
 
   await userEvent.click(screen.getByRole('link', { name: title }));
