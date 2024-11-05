4ab36f102032aab76f8a30a4c97d9f7f587f9e36
Reza Mohseni <49490512+mohsenireza@users.noreply.github.com>
2024-11-05 20:58:23 +0330

    Implement unit test for the Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import Router from './components/Router/Router';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements
+} from 'react-router-dom';
+
+import HomePage from './pages/HomePage/HomePage';
+import RoomPage from './pages/RoomPage/RoomPage';
+import MoviePage from './pages/MoviePage/MoviePage';
+import Layout from './components/Layout/Layout';
 
 function App() {
-  return <Router />;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="room/:roomId" element={<RoomPage />} />
+        <Route path="room/:roomId/movie/:movieId" element={<MoviePage />} />
+        <Route path="*" element={() => null} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
