d2092cf79463718ba935c2479fb9421dfeb67d61
Reza Mohseni <49490512+mohsenireza@users.noreply.github.com>
2024-11-06 20:51:12 +0330

    Reset scroll in pages

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
+
 import { db } from '../../data/db';
+import { resetScroll } from '../../utils/dom';
 import RoomItem from '../../components/RoomItem/RoomItem';
 import './HomePage.css';
 
 function HomePage() {
+  useEffect(() => {
+    resetScroll();
+  }, []);
+
   return (
     <div className="home-page">
       <h1 className="home-page__title">Choose a room</h1>
--- src/pages/MoviePage/MoviePage.jsx
+++ src/pages/MoviePage/MoviePage.jsx
@@ -1,12 +1,18 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { db } from '../../data/db';
+import { resetScroll } from '../../utils/dom';
 import SeatBooker from '../../components/SeatBooker/SeatBooker';
 import MovieItem from '../../components/MovieItem/MovieItem';
 import Header from '../../components/Header/Header';
 
 function MoviePage() {
   const { roomId, movieId } = useParams();
+
+  useEffect(() => {
+    resetScroll();
+  }, []);
 
   const room = db.rooms.find(room => room.id === roomId);
 
